d112ae23bdecccd76fed2cf96b9d8d4b1fbf03ec
LeeThree <leethree9@gmail.com>
2011-04-07 12:41:45 +0800

    Reverted TimeDiff back.

--- src/hk/hku/cs/c7802/base/time/TimeDiff.java
+++ src/hk/hku/cs/c7802/base/time/TimeDiff.java
@@ -1,18 +1,8 @@
 package hk.hku.cs.c7802.base.time;
 
-import hk.hku.cs.c7802.base.BaseException;
-
 public class TimeDiff extends TimeSpan {
 
 	public TimeDiff(long day) {
 		super(0, 0, day);
-	}
-	
-	public int getYear() {
-		throw new RuntimeException("Jiong, TimeDiff is not TimeSpan");
-	}
-
-	public int getMonth() {
-		throw new RuntimeException("Jiong, TimeDiff is not TimeSpan");
 	}
 }
